a0fb854330c662e54acd8339bf1beb47c35ccc0e
Matthew J Roth <mjroth03@louisville.edu>
2020-12-02 11:09:06 -0500

    Add project files.

---
+++ Lab4/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.acceptedOutputLabel = new System.Windows.Forms.Label();
+            this.rejectedOutputLabel = new System.Windows.Forms.Label();
+            this.runningTotalTitle = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.acceptanceLabel = new System.Windows.Forms.Label();
+            this.calculationButton = new System.Windows.Forms.Button();
+            this.admissionScoreInput = new System.Windows.Forms.TextBox();
+            this.gpaInput = new System.Windows.Forms.TextBox();
+            this.admissionScoreLabel = new System.Windows.Forms.Label();
+            this.gpaLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // acceptedOutputLabel
+            // 
+            this.acceptedOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.acceptedOutputLabel.Location = new System.Drawing.Point(502, 118);
+            this.acceptedOutputLabel.Name = "acceptedOutputLabel";
+            this.acceptedOutputLabel.Size = new System.Drawing.Size(128, 18);
+            this.acceptedOutputLabel.TabIndex = 23;
+            // 
+            // rejectedOutputLabel
+            // 
+            this.rejectedOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rejectedOutputLabel.Location = new System.Drawing.Point(502, 191);
+            this.rejectedOutputLabel.Name = "rejectedOutputLabel";
+            this.rejectedOutputLabel.Size = new System.Drawing.Size(128, 18);
+            this.rejectedOutputLabel.TabIndex = 22;
+            // 
+            // runningTotalTitle
+            // 
+            this.runningTotalTitle.AutoSize = true;
+            this.runningTotalTitle.Location = new System.Drawing.Point(499, 66);
+            this.runningTotalTitle.Name = "runningTotalTitle";
+            this.runningTotalTitle.Size = new System.Drawing.Size(77, 13);
+            this.runningTotalTitle.TabIndex = 21;
+            this.runningTotalTitle.Text = "Running Total:";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resultLabel.Location = new System.Drawing.Point(267, 322);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(99, 15);
+            this.resultLabel.TabIndex = 20;
+            // 
+            // acceptanceLabel
+            // 
+            this.acceptanceLabel.AutoSize = true;
+            this.acceptanceLabel.Location = new System.Drawing.Point(108, 322);
+            this.acceptanceLabel.Name = "acceptanceLabel";
+            this.acceptanceLabel.Size = new System.Drawing.Size(126, 13);
+            this.acceptanceLabel.TabIndex = 19;
+            this.acceptanceLabel.Text = "Your Acceptance Status:";
+            // 
+            // calculationButton
+            // 
+            this.calculationButton.Location = new System.Drawing.Point(193, 268);
+            this.calculationButton.Name = "calculationButton";
+            this.calculationButton.Size = new System.Drawing.Size(75, 23);
+            this.calculationButton.TabIndex = 18;
+            this.calculationButton.Text = "Submit";
+            this.calculationButton.UseVisualStyleBackColor = true;
+            this.calculationButton.Click += new System.EventHandler(this.calculationButton_Click);
+            // 
+            // admissionScoreInput
+            // 
+            this.admissionScoreInput.Location = new System.Drawing.Point(266, 207);
+            this.admissionScoreInput.Name = "admissionScoreInput";
+            this.admissionScoreInput.Size = new System.Drawing.Size(100, 20);
+            this.admissionScoreInput.TabIndex = 17;
+            // 
+            // gpaInput
+            // 
+            this.gpaInput.Location = new System.Drawing.Point(266, 138);
+            this.gpaInput.Name = "gpaInput";
+            this.gpaInput.Size = new System.Drawing.Size(100, 20);
+            this.gpaInput.TabIndex = 16;
+            // 
+            // admissionScoreLabel
+            // 
+            this.admissionScoreLabel.AutoSize = true;
+            this.admissionScoreLabel.Location = new System.Drawing.Point(108, 207);
+            this.admissionScoreLabel.Name = "admissionScoreLabel";
+            this.admissionScoreLabel.Size = new System.Drawing.Size(116, 13);
+            this.admissionScoreLabel.TabIndex = 15;
+            this.admissionScoreLabel.Text = "Enter Admission Score:";
+            // 
+            // gpaLabel
+            // 
+            this.gpaLabel.AutoSize = true;
+            this.gpaLabel.Location = new System.Drawing.Point(108, 141);
+            this.gpaLabel.Name = "gpaLabel";
+            this.gpaLabel.Size = new System.Drawing.Size(60, 13);
+            this.gpaLabel.TabIndex = 14;
+            this.gpaLabel.Text = "Enter GPA:";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(190, 66);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(104, 13);
+            this.titleLabel.TabIndex = 13;
+            this.titleLabel.Text = "Admission Calculator";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.acceptedOutputLabel);
+            this.Controls.Add(this.rejectedOutputLabel);
+            this.Controls.Add(this.runningTotalTitle);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.acceptanceLabel);
+            this.Controls.Add(this.calculationButton);
+            this.Controls.Add(this.admissionScoreInput);
+            this.Controls.Add(this.gpaInput);
+            this.Controls.Add(this.admissionScoreLabel);
+            this.Controls.Add(this.gpaLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label acceptedOutputLabel;
+        private System.Windows.Forms.Label rejectedOutputLabel;
+        private System.Windows.Forms.Label runningTotalTitle;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Label acceptanceLabel;
+        private System.Windows.Forms.Button calculationButton;
+        private System.Windows.Forms.TextBox admissionScoreInput;
+        private System.Windows.Forms.TextBox gpaInput;
+        private System.Windows.Forms.Label admissionScoreLabel;
+        private System.Windows.Forms.Label gpaLabel;
+        private System.Windows.Forms.Label titleLabel;
+    }
+}
+
---
+++ Lab4/Form1.cs
@@ -0,0 +1,106 @@
+﻿//Grading ID: K1791
+//Lab 4
+//27 September 2020
+//CIS 199-02
+//This lab checks to see if the gpa and test scores of a student will allow them to be enrolled into college.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+
+        const double lowestGpaPossible = 3.0; //The lowest possible threshhold for GPA, but only if the user has a test score of at least  60.
+        const double lowestTestScorePossible = 60; //The lowest possible gpa for a student with a test score of atleast 60.
+        const double highestTestScorePossible = 80; //The highest threshhold of test score, but only if a student has a gpa of less than 3.0.
+
+        private int howManyRejections = 0; //Storing a value for how many rejections there are
+        private int howManyAcceptances = 0; //Storing a value for how many acceptances there are
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        
+        //Sets the value of the running totals to zero at the start of the program, and displays "accepted" and "rejected".
+        private void Form1_Load(object sender, EventArgs e) 
+        {
+            SetLabel(acceptedOutputLabel, howManyAcceptances, "Acceptances"); //Displays the default value of 0 for how many acceptances, and displays "Total Acceptances".
+            SetLabel(rejectedOutputLabel, howManyRejections, "Rejections"); //Displays the default value of 0 for how many rejections, and displays "Rejected".
+        }
+
+        //Sets the value for all labels in the form
+        private void SetLabel(Label label, int amount, string type)
+        {
+            label.Text = amount + " - " + type; //Adds together each entry on the running total.
+        }
+
+        //The main calculations for this program are done here once the calculation button is clicked.
+        private void calculationButton_Click(object sender, EventArgs e)
+        {
+            double userGpa;  //GPA of user
+
+            double userAdmissionScore; //Admission score of user
+            _ = double.TryParse(gpaInput.Text, out userGpa); //Checks if the user has input something
+            _ = double.TryParse(admissionScoreInput.Text, out userAdmissionScore); //Checks if the user has input something
+
+
+
+            bool studentHasBeenAccepted = (userGpa >= lowestGpaPossible && userAdmissionScore >= lowestTestScorePossible) //The set of values that will lead to acceptance
+               || (userGpa < lowestGpaPossible && userAdmissionScore >= highestTestScorePossible); //The set of values that will lead to acceptance
+
+
+            if (userGpa < 0) 
+            {
+                MessageBox.Show("Error, GPA must be between 0.0-4.0, and Test score must be between 0-100");
+                gpaInput.Text = ""; //Clears the error(s) for the user
+                admissionScoreInput.Text = ""; //Clears the error(s) for the user
+            }
+            else if (userGpa > 4.0)
+                {
+                MessageBox.Show("Error, GPA must be between 0.0-4.0, and Test score must be between 0-100");
+                gpaInput.Text = ""; //Clears the error(s) for the user
+                admissionScoreInput.Text = ""; //Clears the error(s) for the user
+            }
+            
+            
+            else if (userAdmissionScore < 0 )
+            {
+                MessageBox.Show("Error, GPA must be between 0.0-4.0, and Test score must be between 0-100");
+                admissionScoreInput.Text = ""; //Clears the error(s) for the user
+                gpaInput.Text = ""; //Clears the error(s) for the user
+            }
+            
+                else if (userAdmissionScore > 100)
+                    {
+                MessageBox.Show("Error, GPA must be between 0.0-4.0, and Test score must be between 0-100");
+                admissionScoreInput.Text = ""; //Clears the error(s) for the user
+                gpaInput.Text = ""; //Clears the error(s) for the user
+            }
+
+             else if (studentHasBeenAccepted) //If the input values match up:
+            {
+                resultLabel.Text = "Accept"; //Displays this message
+                SetLabel(acceptedOutputLabel, ++howManyAcceptances, "Acceptances"); //Updates the running total output for acceptances
+            }
+              else 
+            {
+                resultLabel.Text = "Reject"; //Displays this message
+                SetLabel(rejectedOutputLabel, ++howManyRejections, "Rejections"); //Updates the running total output for rejections
+            }
+        }
+    }
+    
+
+}
